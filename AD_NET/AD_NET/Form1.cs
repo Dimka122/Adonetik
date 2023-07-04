80a1d0d4bf689f804564494947b7643af2abe89b
Дмитрий Кассин <kassindima1@gmail.com>
2023-07-04 15:33:51 +0300

    ///////\\\\\\\\\\\\

--- AD_NET/AD_NET/Form1.cs
+++ AD_NET/AD_NET/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,51 @@ namespace AD_NET
 {
     public partial class Form1 : Form
     {
+        ProductContext _context;
         public Form1()
         {
             InitializeComponent();
         }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            _context = new ProductContext();
+
+            _context.Categories.Load();
+
+            this.categoryBindingSource.DataSource =
+                _context.Categories.Local.ToBindingList();
+        }
+
+        private void categoryBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+
+            foreach (var product in _context.Products.Local.ToList())
+            {
+                if (product.Category == null)
+                {
+                    _context.Products.Remove(product);
+                }
+            }
+
+            this._context.SaveChanges();
+
+            this.categoryDataGridView.Refresh();
+            this.productsDataGridView.Refresh();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            this._context.Dispose();
+        }
+    
+
+    //private void categoryBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+       // {
+
+        //}
     }
 }
